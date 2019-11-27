8b8068e9fc9d22789a4840f50c520cc6be784d34
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-11-27 12:17:23 -0800

    Boolean Logic Exercise
    Submitted Boolean Logic Exercise step 75

---
+++ BooleanLogicStep75/BooleanLogicStep75/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicStep75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? ");
+            int x;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            bool TrueOrFalse = x >= 15;
+
+            string DUI = null;
+            Console.WriteLine("Have you ever had a DUI?");
+            DUI = Console.ReadLine();
+            if (DUI == "no")
+            {
+                Console.WriteLine("False");
+            }
+            else if (DUI == "yes")
+            {
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int y;
+
+            y = Convert.ToInt32(Console.ReadLine());
+            bool YesOrNo = y <= 3;
+
+            Console.ReadLine();
+
+            if (x >= 15)
+                if (DUI == "no")
+                    if (y <= 3)
+                    {
+                        Console.WriteLine("Applicant is qualified.");
+                    }
+
+                    else if (y >= 3)
+                    {
+                        Console.WriteLine("Applicant is not qualified.");
+                    }
+
+                    else if (DUI == "yes")
+                    {
+                        Console.WriteLine("Applicant is not qualified.");
+                    }
+
+            if (x < 15)
+            {
+                Console.WriteLine("Applicant is not qualified.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
