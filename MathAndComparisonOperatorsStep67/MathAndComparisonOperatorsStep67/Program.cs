f2efe33ca201c851f522eaadc5d80ee2ae093d98
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-11-27 09:14:02 -0800

    Math and Comparison Operators Exercise
    Submitted Math and Comparison Operators Exercise Step 67

---
+++ MathAndComparisonOperatorsStep67/MathAndComparisonOperatorsStep67/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperatorsStep67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program: ");
+
+            Console.WriteLine("Person 1:");
+            int hourlyRatePerson1;
+            Console.WriteLine("Input hourly rate: ");
+            hourlyRatePerson1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hourly Rate: " + hourlyRatePerson1);
+            Console.WriteLine("Hours worked per week: " + 40);
+            Console.ReadLine();
+
+            Console.WriteLine("Person 2:");
+            int hourlyRatePerson2;
+            Console.WriteLine("Input hourly rate: ");
+            hourlyRatePerson2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hourly Rate: " + hourlyRatePerson2);
+            Console.WriteLine("Hours worked per week: " + 40);
+            Console.ReadLine();
+
+            Console.WriteLine("Weekly salary of Person 1: " + hourlyRatePerson1 * 40);
+            Console.WriteLine("Weekly salary of Person 2: " + hourlyRatePerson2 * 40);
+
+            bool TrueOrFalse = hourlyRatePerson1 > hourlyRatePerson2;
+            Console.WriteLine("Does Person 1 make more money than Person 2?" + TrueOrFalse.ToString());
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
