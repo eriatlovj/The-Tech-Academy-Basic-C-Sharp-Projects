9c3ab4b89b0256276d0277a794f37d9a43b7e471
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-11-28 16:36:33 -0800

    Branching Exercise
    Submitted Branching Exercise Step 92

---
+++ BranchingDrillStep92/BranchingDrillStep92/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingDrillStep92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight: ");
+            int PackageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (PackageWeight >= 51)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("Please enter the package width: ");
+            int PackageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height: ");
+            int PackageHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length: ");
+            int PackageLength = Convert.ToInt32(Console.ReadLine());
+
+            int total = PackageHeight + PackageWidth + PackageLength;
+            if (total >= 51)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+            else
+
+            {
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+            int ShippingTotal1 = total * PackageWeight;
+            int ShippingTotal2 = ShippingTotal1 % 100;
+            {
+                Console.WriteLine("Your estimated total for shipping this package is: $" + ShippingTotal2);
+            }
+
+            Console.WriteLine("Thank you.");
+            Console.ReadLine();
+        }
+    }
+}
