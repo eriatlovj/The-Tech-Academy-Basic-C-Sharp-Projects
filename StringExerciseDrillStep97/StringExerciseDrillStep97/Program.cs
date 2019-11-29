5e16b23a1d4fe5267543fa3e38d8b4a12a80727c
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-11-28 21:38:04 -0800

    String Exercise Step 97
    Submitted string exercise for step 97

---
+++ StringExerciseDrillStep97/StringExerciseDrillStep97/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExerciseDrillStep97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "Egg man";
+            string UserName2 = "Walrus";
+            UserName = UserName.ToUpper();
+            UserName2 = UserName2.ToUpper();
+            string dateString = DateTime.Today.ToShortDateString();
+            string str = "Hello " + UserName + ". " + "Hi " + UserName2 + ". Today is " + dateString + ". " + "It's almost the end of the month.";
+            Console.WriteLine(str);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("I am the egg man.");
+            sb.AppendLine(" They are the egg men.");
+            sb.AppendLine("I am the walrus.");
+            sb.AppendLine("Goo goo g'joob.");
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+
+        }
+    }
+}
