fe4717562f9a2c8bf6ae252a5dc87bde9ddcebaf
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-11-28 18:13:42 -0800

    Exercise for Step 94
    Submitting exercise for Step 94:
    While Statement and Do While Statement.

---
+++ DoWhileStatementStep94/DoWhileStatementStep94/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileStatementStep94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Counting from 10 to 100...");
+            int num = 10;
+            Console.ReadLine();
+            do
+
+            {
+                Console.WriteLine("Value of num: " + num);
+                num++;
+            }
+
+            while (num <= 100);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileStatementStep94/WhileStatementStep94/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileStatementStep94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count from 10 to 100: ");
+            int i = 10;
+            while (i <= 100)
+            {
+                Console.WriteLine("Count: "+ i);
+                i++;
+                Console.ReadLine();
+            }
+        }
+    }
+}
