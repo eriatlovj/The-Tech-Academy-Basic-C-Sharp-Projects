f29524f94eb85d34cd9026125795de360c77a13e
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-11-30 06:22:58 -0800

    Array and String Exercise
    Submitted Array and String exercise step 100

---
+++ ArrayStringsStep100/ArrayStringsStep100/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStringsStep100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Select an index from 0 to 4. \nThen, the console will display the equivalent value of that index.");
+                string[] stringArray = { "cat", "dog", "mouse", "fish", "turtle" };
+                int string1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The value of that index is: " + stringArray[string1]);
+                Console.ReadLine();
+
+                Console.WriteLine("Select an index from 0 to 4. \nThen, the console will display the equivalent value of that index.");
+                int[] numArray = { 5, 21, 19, 86, 12 };
+                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine("The value of that index is: " + numArray[input]);
+                Console.ReadLine();
+
+                Console.WriteLine("Select an index from 0 to 3. \nThen, the console will display the equivalent value of that index.");
+                List<string> list1 = new List<string>() { "John", "Paul", "George", "Ringo" };
+                int list2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The value of that index is: " + list1[list2]);
+                Console.ReadLine();
+
+            }
+
+            catch (System.IndexOutOfRangeException)
+
+            {
+                Console.WriteLine("Selected index does not exist.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
