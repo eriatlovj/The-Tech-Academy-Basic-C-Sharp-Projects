521d81a183fe252b3533eb6d6dbab7352d1f1a18
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-02 14:00:45 -0800

    Try/Catch Block Drill Step 106
    Submitted Try/Catch Block Drill Step 106

---
+++ tryCatchBlockStep106/tryCatchBlockStep106/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchBlockStep106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listNumber= new List<int>();
+            int[] numbers = {8, 24, 34, 23, 45};
+            listNumber.AddRange(numbers);
+
+            try
+            {
+                Console.WriteLine("Please type a number and it will be used to divide the numbers listed above.");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                int result = 0;
+
+                foreach (int number in listNumber)
+                {
+                    result = number / userNumber;
+                    Console.WriteLine(result);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That is not a number. Please try again.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please do not use the number zero.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry. Something went wrong.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
