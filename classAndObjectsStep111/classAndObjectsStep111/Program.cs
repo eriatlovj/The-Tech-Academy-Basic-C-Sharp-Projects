8ee5d0bc12a8247c9b420ebee43518452e2e5d62
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-02 22:15:46 -0800

    Class, Object and Methods Drill Step 111
    Submitted Class, Object and Methods Drill Step 111

---
+++ classAndObjectsStep111/classAndObjectsStep111/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAndObjectsStep111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass math1 = new mathClass();
+
+            Console.WriteLine("Please provide a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            userInput = math1.MathMethod1(userInput);
+
+            Console.WriteLine("Running Math Method 1 to add 15, your number becomes " + userInput);
+            Console.ReadLine();
+            userInput = math1.MathMethod2(userInput);
+            Console.WriteLine("Running Math Method 2 to multiply by 4, your number becomes " + userInput);
+            Console.ReadLine();
+            userInput = math1.MathMethod3(userInput);
+            Console.WriteLine("Running Math Method 3 to subtract by 8, your number approximately becomes " + userInput);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ classAndObjectsStep111/classAndObjectsStep111/mathClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAndObjectsStep111
+{
+    class mathClass
+    {
+        public mathClass()
+        {
+            Value = 1;
+        }
+
+        public int MathMethod1(int userInput)
+        {
+            userInput = userInput + 15;
+
+            return userInput;
+        }
+
+        public int MathMethod2(int userInput)
+        {
+            userInput = userInput * 4;
+
+            return userInput;
+        }
+
+        public int MathMethod3(int userInput)
+        {
+            userInput = userInput - 8;
+
+            return userInput;
+        }
+        public int Value { get; set; }
+    }
+}
