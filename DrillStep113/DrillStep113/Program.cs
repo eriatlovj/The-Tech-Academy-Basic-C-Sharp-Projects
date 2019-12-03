e80bddf9c431d75078e62585b6c15b9eb1980cc8
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-03 15:52:07 -0800

    Drill Step 113
    Submitted drill for step113

---
+++ DrillStep113/DrillStep113/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            int parameter1 = 20;
+            int parameter2 = 40;
+            testClass.voidMethod(parameter1, parameter2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DrillStep113/DrillStep113/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep113
+{
+    class TestClass
+    {
+        public void voidMethod(int parameter1, int parameter2)
+        {
+            int number = parameter1 + 20;
+            Console.WriteLine("The second parameter is: " + parameter2 + ".");
+        }
+    }
+}
