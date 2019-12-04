0ff98a820763646db6b05d5532a7ddd13d23b4be
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-03 21:59:53 -0800

    Drill Step 115
    Submitting Drill Step 115

---
+++ DrillStep115/DrillStep115/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass Example = new TestClass();
+            Console.WriteLine("Please type a whole number.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type in a new whole number or you may just leave it blank.");
+            string number2 = Console.ReadLine();
+            if (number2 == "")
+            {
+                Example.TestMethod(number1);
+            }
+            else 
+            {
+                int int2 = Convert.ToInt32(number2);
+                Example.TestMethod(number1, int2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DrillStep115/DrillStep115/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep115
+{
+    class TestClass
+    {
+        public void TestMethod(int number1, int number2 = 20)
+        {
+            int result = number1 + number2;
+            Console.WriteLine(result);
+        }
+    }
+}
