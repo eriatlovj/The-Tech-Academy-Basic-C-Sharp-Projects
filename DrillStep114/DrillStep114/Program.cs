dcfc51e741ae3d7f8e75613c79ff6c3494fba223
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-03 18:44:04 -0800

    Drill Step 114
    Submitted Drill Step 114

---
+++ DrillStep114/DrillStep114/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+
+            int userInteger = 21;
+            double userDecimal = 0.3125;
+            string userString = "160";
+
+            userInteger = testClass.testMethod(userInteger);
+
+            userDecimal = testClass.testMethod(userDecimal);
+
+            userString = testClass.testMethod(userString);
+
+            Console.WriteLine("The values returned are: " + userInteger + ", " + userDecimal + " and " + userString);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DrillStep114/DrillStep114/TestClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep114
+{
+    class TestClass
+    {
+        public int testMethod(int userInteger)
+        {
+            userInteger = userInteger + 10;
+            return userInteger;
+        }
+
+        public double testMethod(double userDecimal)
+        {
+            userDecimal = userDecimal * 4;
+            return userDecimal;
+        }
+        public string testMethod(string userString)
+        {
+            int temp = Convert.ToInt32(userString);
+            temp = temp / 2;
+
+            userString = Convert.ToString(temp);
+
+            return userString;
+        }
+
+
+    }
+}
