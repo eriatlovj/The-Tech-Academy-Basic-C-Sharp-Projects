398bfc876c02b779ff384f624d440e9a887c0643
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-04 09:57:16 -0800

    Drill Step 117
    Submitted Drill Step 117

---
+++ DrillStep117/DrillStep117/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in an integer to be divided by 2.");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            TestClass.MathMethod(i, out int j);
+            Console.WriteLine("Your Integer divided by 2 is equals to: " + j);
+            Console.ReadLine();
+
+            Console.WriteLine("Type in two more integers.");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int l = Convert.ToInt32(Console.ReadLine());
+            TestClass.MathMethod(i,k,l, out int sum);
+            Console.WriteLine("The sum of all numbers are: " + sum);
+            Console.ReadLine();
+
+            TestClass.Addition(out int m);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DrillStep117/DrillStep117/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep117
+{
+    class TestClass
+    {
+        public static void MathMethod(int j, out int result)
+        {
+            result = j / 2;
+        }
+
+        public static void MathMethod(int i, int k, int l, out int sum)
+        {
+            sum = i + k + l; 
+        }
+
+        public static void Addition(out int m)
+        {
+            m = 20;
+            m += m;
+            Console.WriteLine("Here is the output method: " + m);
+        }
+
+    }
+}
