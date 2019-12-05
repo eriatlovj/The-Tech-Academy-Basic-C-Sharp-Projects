e08792f8af9f04098060ea8a61922cbda563c2e2
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-04 19:46:02 -0800

    Drill Step 120
    Submitted Drill for Step 120

---
+++ DrillStep120/DrillStep120/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep120
+{
+    public class Person
+    {
+
+        public List<string> Employee { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void sayName()
+            {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }  
+    }
+}
---
+++ DrillStep120/DrillStep120/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.sayName();
+            Console.ReadLine();
+        }
+    }
+}
