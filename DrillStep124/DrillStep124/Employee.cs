deeb155816be7ef0a6b42ec03fb345c18621a671
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-05 08:24:40 -0800

    Drill Step 124
    Submitted Drill Step 124

---
+++ DrillStep124/DrillStep124/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep124
+{
+    class Employee : Person
+    {
+        public override void sayName()
+        {
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+        }
+
+    }
+}
