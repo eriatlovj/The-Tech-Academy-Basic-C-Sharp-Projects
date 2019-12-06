93080ca4a7caaf3bbe30735f610bb57f5f158d06
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-05 16:45:41 -0800

    code correction

--- DrillStep126/DrillStep124/Program.cs
+++ DrillStep126/DrillStep124/Program.cs
@@ -11,37 +11,14 @@ namespace DrillStep124
         static void Main(string[] args)
         {
 
-            Employee<string> thingsString = new Employee<string>();
-            thingsString.Things = new List<string>();
-            thingsString.Things.Add("Thing One");
-            thingsString.Things.Add("Thing Two");
-            thingsString.Things.Add("Thing Three");
+            IQuittable Quit = new Employee();
+            Quit.Quit();
+            Employee employee = new Employee();
 
-            Employee<int> IntThings = new Employee<int>();
-            IntThings.Things = new List<int>();
-            IntThings.Things.Add(1);
-            IntThings.Things.Add(2);
-            IntThings.Things.Add(3);
-
-            foreach (string Items in thingsString.Things)
-            {
-                Console.WriteLine(Items);
-                Console.ReadLine();
-            }
-
-            foreach (int Item in IntThings.Things)
-            {
-                Console.WriteLine(Item);
-                Console.ReadLine();
-            }
-
-
-
-            //Employee employee = new Employee();
-            //employee.firstName = "Sample";
-            //employee.lastName = "Student";
-            //employee.sayName();
-            //Console.ReadLine();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.sayName();
+            Console.ReadLine();
         }
     }
 }
