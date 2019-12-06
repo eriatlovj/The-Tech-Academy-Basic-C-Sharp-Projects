94570eaa27141dbab5d1d5c7aa1b10cf8077edc6
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-06 09:28:40 -0800

    Drill Step 131
    Submitted drill for step 131

---
+++ DrillStep131/DrillStep131/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep131
+{
+    class Employee<T> : Person, IQuittable
+    {
+        public List<T> Things;
+
+        public void Quit()
+        { 
+        
+        }
+    }
+}
---
+++ DrillStep131/DrillStep131/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> ThingsString = new Employee<string>();
+            ThingsString.Things = new List<string>();
+            ThingsString.Things.Add("Thing one");
+            ThingsString.Things.Add("Thing two");
+            ThingsString.Things.Add("Thing three");
+
+            Employee<int> IntThings = new Employee<int>();
+            IntThings.Things = new List<int>();
+            IntThings.Things.Add(3);
+            IntThings.Things.Add(2);
+            IntThings.Things.Add(1);
+
+            foreach (string Item1 in ThingsString.Things)
+            {
+                Console.WriteLine(Item1);
+                Console.ReadLine();
+            }
+
+            foreach (int Item2 in IntThings.Things)
+            {
+                Console.WriteLine(Item2);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
