cd647f115b539f8ec9809a2a3695b46b152aec21
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-05 18:41:30 -0800

    Drill Step 129
    Submitted Drill for Step 129

---
+++ DrillStep129/DrillStep129/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep129
+{
+    class Employee : Person, IQuittable
+    {
+        public int ID;
+        public int ID1;
+
+        public static bool operator ==(Employee ID, Employee ID1)
+        {
+            bool isEqual = ID == ID1;
+            return true;
+        }
+        public static bool operator !=(Employee ID, Employee ID1)
+        {
+            bool notEqual = ID != ID1;
+            return false;
+        }
+
+        public override void sayName()
+        {
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+        }
+
+        public void Quit()
+        {
+
+        }
+}
+}
---
+++ DrillStep129/DrillStep129/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep129
+{
+    abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public string firstName1;
+        public string lastName1;
+
+        public abstract void sayName();
+    }
+}
+
