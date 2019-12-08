bc3dcfd7cf7a2b238eadec99294404e491d9f9ae
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-08 07:43:59 -0800

    Drill Step 156
    Submitted drill for step 156

---
+++ DrillStep156/DrillStep156/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("Current time is: " + currentTime);
+            Console.WriteLine("Provide a number equivalent to a number of hours to be added to the current time.");
+            int additionalHours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime addedToCurrentTime = currentTime.AddHours(additionalHours);
+            Console.WriteLine("The updated time is: " + addedToCurrentTime);
+
+            Console.ReadLine();
+        }
+    }
+}
