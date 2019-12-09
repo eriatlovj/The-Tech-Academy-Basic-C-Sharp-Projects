74e2b356dff3e6069e6813bc87fa2656c2224dde
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-08 16:09:20 -0800

    Drill Step 162
    Submitted drill for step 162

---
+++ DrillStep162/DrillStep162/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillStep162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string mathematics = "I'm bad in math.";
+            var name = "J";
+
+            Console.WriteLine("Hello, I'm  " + name + " and " + mathematics);
+            name = name + "O";
+            Console.WriteLine("Hello, I'm " + name + " and " + mathematics);
+
+            Console.ReadLine();
+
+            Person firstPerson = new Person("J", "O");
+            var secondPerson = new Person("J.O.");
+            
+            Console.WriteLine(firstPerson.FirstName + " " + firstPerson.LastName);
+            Console.WriteLine(secondPerson.FirstName + " " + secondPerson.LastName);
+
+            Console.ReadLine();
+
+        }
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+
+            public Person(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+            public Person(string firstName) : this(firstName, "Jo")
+            {
+
+            }
+
+        }
+    }
+}
